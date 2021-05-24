689a99ef8baa2007049d9d3d8a1d1cccf4b73019
Wade Pate <wadejp8@gmail.com>
2021-05-24 10:27:31 -0600

    Added Delete and Edit for Restaurant.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,76 @@
-import React, { useState } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+/** @format */
 
-import AddReview from "./components/AddReview";
-import Login from "./components/Login";
-import Restaurant from "./components/Restaurant";
-import RestaurantsList from "./components/RestaurantsList";
+import React, { useState } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from './components/AddReview';
+import Login from './components/Login';
+import Restaurant from './components/Restaurant';
+import RestaurantsList from './components/RestaurantsList';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
 
-  async function login(user = null) {
-    setUser(user);
-  }
+	async function login(user = null) {
+		setUser(user);
+	}
 
-  async function logout() {
-    setUser(null);
-  }
+	async function logout() {
+		setUser(null);
+	}
 
-  return (
-    <div>
-      <nav className='navbar navbar-expand navbar-dark bg-dark'>
-        <a href='/restaurants' className='navbar-brand'>
-          Restaurant Reviews
-        </a>
-        <div className='navbar-nav mr-auto'>
-          <li className='nav-item'>
-            <Link to={"/restaurants"} className='nav-link'>
-              Restaurants
-            </Link>
-          </li>
-          <li className='nav-item'>
-            {user ? (
-              <Link
-                to={"/login"}                
-                onClick={logout}
-                className='nav-link'
-                style={{ cursor: "pointer" }}>
-                Logout {user.name}
-              </Link>
-            ) : (
-              <Link to={"/login"} className='nav-link'>
-                Login
-              </Link>
-            )}
-          </li>
-        </div>
-      </nav>
+	return (
+		<div>
+			<nav className="navbar navbar-expand navbar-dark bg-dark">
+				<a href="/restaurants" className="navbar-brand">
+					Restaurant Reviews
+				</a>
+				<div className="navbar-nav mr-auto">
+					<li className="nav-item">
+						<Link to={'/restaurants'} className="nav-link">
+							Restaurants
+						</Link>
+					</li>
+					<li className="nav-item">
+						{user ? (
+							<Link
+								to={'/login'}
+								onClick={logout}
+								className="nav-link"
+								style={{ cursor: 'pointer' }}
+							>
+								Logout {user.name}
+							</Link>
+						) : (
+							<Link to={'/login'} className="nav-link">
+								Login
+							</Link>
+						)}
+					</li>
+				</div>
+			</nav>
 
-      <div className='container-mt-3'>
-        <Switch>
-          <Route
-            exact
-            path={["/", "/restaurants"]}
-            component={RestaurantsList}
-          />
-          <Route
-            path='/restaurants/:id/review'
-            render={(props) => <AddReview {...props} user={user} />}
-          />
-          <Route
-            path='/restaurants/:id'
-            render={(props) => <Restaurant {...props} user={user} />}
-          />
-          <Route
-            path='/login'
-            render={(props) => <Login {...props} login={login} />}
-          />
-        </Switch>
-      </div>
-    </div>
-  );
+			<div className="container-mt-3">
+				<Switch>
+					<Route exact path={['/', '/restaurants']} component={RestaurantsList} />
+					<Route
+						path="/restaurants/:id/review"
+						render={(props) => <AddReview {...props} user={user} />}
+					/>
+					<Route
+						path="/restaurants/:id"
+						render={(props) => <Restaurant {...props} user={user} />}
+					/>
+					<Route
+						path="/login"
+						render={(props) => <Login {...props} login={login} />}
+					/>
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/Restaurant.js
+++ frontend/src/components/Restaurant.js
@@ -1,7 +1,131 @@
-import React from "react";
+/** @format */
 
-const Restaurant = () => {
-  return <div>Restaurant Component</div>;
+import React, { useState, useEffect } from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom';
+
+const Restaurant = (props) => {
+	const initialRestaurantState = {
+		id: null,
+		name: '',
+		address: {},
+		cuisine: '',
+		reviews: [],
+	};
+	const [restaurant, setRestaurant] = useState(initialRestaurantState);
+
+	const getRestaurant = (id) => {
+		RestaurantDataService.get(id)
+			.then((res) => {
+				setRestaurant(res.data);
+				console.log(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	useEffect(() => {
+		getRestaurant(props.match.params.id);
+	}, [props.match.params.id]);
+
+	const deleteReview = (reviewId, i) => {
+		RestaurantDataService.deleteReview(reviewId)
+			.then((res) => {
+				setRestaurant((prevState) => {
+					prevState.reviews.splice(i, 1);
+					return {
+						...prevState,
+					};
+				});
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	return (
+		<div className="container">
+			{restaurant ? (
+				<div>
+					<h5>{restaurant.name}</h5>
+					<p>
+						<strong>Cuisine: </strong>
+						{restaurant.cuisine}
+						<br />
+						<strong>Address: </strong>
+						{restaurant.address.building} {restaurant.address.street},{' '}
+						{restaurant.address.zipcode}
+					</p>
+					<Link
+						to={'/restaurants/' + props.match.params.id + '/review'}
+						className="btn btn-primary"
+					>
+						Add a Review
+					</Link>
+					<h4 style={{ display: 'flex', justifyContent: 'center', fontSize: 50 }}>
+						Reviews for {restaurant.name} at {restaurant.address.street}
+					</h4>
+					<div className="row">
+						{restaurant.reviews.length > 0 ? (
+							restaurant.reviews.map((review, i) => {
+								return (
+									<div className="col-lg-4 pb-1" key={i}>
+										<div className="card">
+											<div className="card-body">
+												<p className="card-text">
+													{review.text}
+													<br />
+													<strong>User: </strong>
+													{review.name}
+													<br />
+													<strong>Date: </strong>
+													{review.date}
+												</p>
+												{props.user && props.user.id === review.user_id && (
+													<div className="row">
+														<button
+															onClick={() => deleteReview(review._id, i)}
+															className="btn btn-primary col-lg-5 mx-1 mb-1"
+														>
+															Delete
+														</button>
+														<Link
+															to={{
+																pathname:
+																	'/restaurants/' +
+																	props.match.params.id +
+																	'/review',
+																state: {
+																	currentReview: review,
+																},
+															}}
+															className="btn btn-primary col-lg-5 mx-1 mb-1"
+														>
+															Edit
+														</Link>
+													</div>
+												)}
+											</div>
+										</div>
+									</div>
+								);
+							})
+						) : (
+							<div className="col-sm-4">
+								<h4>No Reviews Yet</h4>
+							</div>
+						)}
+					</div>
+				</div>
+			) : (
+				<div>
+					<br />
+					<h3>No Restaurant Selected</h3>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Restaurant;
